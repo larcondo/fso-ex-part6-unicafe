fdb3ade6ca99313efb81bc57a828aae71e8bd8c8
larcondo <larcondo90@gmail.com>
2023-10-10 15:06:48 -0300

    exercise 6.2

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,20 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+  const incGood = () => store.dispatch({ type: 'GOOD' })
+  const incOk = () => store.dispatch({ type: 'OK' })
+  const incBad = () => store.dispatch({ type: 'BAD' })
+  const reset = () => store.dispatch({ type: 'ZERO' })
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={incGood}>good</button> 
+      <button onClick={incOk}>ok</button> 
+      <button onClick={incBad}>bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
